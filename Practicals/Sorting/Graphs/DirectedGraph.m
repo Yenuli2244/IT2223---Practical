4db28e03945d678378bf80605a1ea5bdf67969dc
Yenuli2244 <yenuliminsadi86@gmail.com>
2025-06-04 09:45:59 +0530

    Add files via upload

---
+++ Practicals/Sorting/Graphs/DirectedGraph.m
@@ -0,0 +1,7 @@
+G = digraph([1,2], [2,3]); %edges: 1->2 and
+plot(G);
+title('Directed Graph');
+
+GH = digraph([2,1],[2,3]);
+plot(GH);
+title('Directed Graph 01');
---
+++ Practicals/Sorting/Graphs/UnweightedGraph.m
@@ -0,0 +1,3 @@
+g = graph([1 2 3], [2 3 4]);
+plot(G);
+title('Unweighted Graph');
